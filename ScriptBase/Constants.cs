211ea39191fad41888c24649a69ccbf49ac83f71
Cameron Berry <camberry@telus.net>
2017-01-27 18:45:32 -0700

    Restructured base script implementation. More additions the the base script
    Restructured the base implementation so that the updating of any script
    components is handled explicitly by the base script thread. Other
    changes all around.

--- ScriptBase/Constants.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace AirSuperiority
-{
-    public static class Constants
-    {
-        public const int MaxPlayers = 12;
-        public const int MaxConcurrentTeams = 4;
-    }
-}
--- ScriptBase/Types/AI/AIState.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace AirSuperiority.ScriptBase.Types
-{
-    public struct AIState
-    {
-        public AIStatus Status { get; set; }
-        public int TimeToNextDecision { get; set; }
-    }
-}
